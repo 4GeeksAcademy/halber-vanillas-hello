7cddc068b692b2703b638508e6e028a0e927227c
Alberto Fernández <albertofernandezfernandez01@gmail.com>
2025-05-06 19:32:57 +0000

    Por terminar

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+window.onload = () => {
+  let pronouns = ['the', 'our'];
+  let adjectives = ['great', 'big'];
+  let nouns = ['jogger', 'racoon'];
+  let domains = [".com", ".es"]
+  let allDomain = []
+
+  for (let pronoun = 0;  pronoun < pronouns.length; pronoun++) {
+    for (let adjetive = 0; djetive < adjectives.length; adjetive++) {
+      for (let noun = 0; noun < nouns.length; noun++) {
+        for (let domain = 0; domain < domains.length; domain++) {
+          allDomain.push(pronouns[pronoun] + adjectives[adjetive] + nouns[noun] + domains[domain]);
+        }
+      }
+    }
+  }
+  let domainList = document.getElementById('domain-list');
+  domainList.style.listStyle = "none";
+  domainList.style.textAlign = "center";
+  domainList.innerHTML = allDomain.map(domain => `<li>${domain}</li>`).join('');
 };
